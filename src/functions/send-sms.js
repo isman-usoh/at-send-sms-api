72cc52ee7b934b218b0598e8f0a93d2e5976bd98
Isman Usoh <isman.usoh@gmail.com>
2019-11-14 12:08:05 +0700

    Update send-sms.js

--- src/functions/send-sms.js
+++ src/functions/send-sms.js
@@ -7,26 +7,22 @@ export async function sendSms(to, msg, host = '192.168.1.1', post = 8091) {
         var client = new net.Socket();
         client.connect(post, host, function() {
             console.log('Connected');
-            delay(() => {
+            setTimeout(function(){
                 client.write('AT+CMGF=1\r')
-
-                delay(() => {
-                    client.write('AT+CMGS=\"' + to + '\"\r')
-
-                    delay(() => {
+                setTimeout(function(){
+                    client.write(`AT+CMGS=\"${to}\"\r`)
+                    setTimeout(function(){
                         client.write(msg + '\r')
-
-                        delay(() => {
+                        setTimeout(function(){
                             client.write(new Buffer[0x1a]);
                             client.write('\x1A')
-                            client.destroy()
-                            resolve()
-                        })
-                    })
-                })
-            })
+                            client.close();
+                        }, 4000);
+                    }, 4000);
+                }, 4000);
+            }, 4000);
         });
-    
+            
         client.on('error', function(error) {
             console.log('Error: ' + error);
             reject(error)
@@ -34,6 +30,7 @@ export async function sendSms(to, msg, host = '192.168.1.1', post = 8091) {
     
         client.on('close', function() {
             console.log('Connection closed');
+            resolve()
         });
     })
 }
